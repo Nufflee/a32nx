25ccc402d88e36644df5194bab5aa21f3e932aef
BBK <22713769+BlueberryKing@users.noreply.github.com>
2023-01-22 10:35:59 +0100

    feat: vertical navigation v1 #7080 (@930841d)

--- src/instruments/src/EFB/Settings/Pages/RealismPage.tsx
+++ src/instruments/src/EFB/Settings/Pages/RealismPage.tsx
@@ -17,9 +17,11 @@ export const RealismPage = () => {
     const [adirsAlignTime, setAdirsAlignTime] = usePersistentProperty('CONFIG_ALIGN_TIME', 'REAL');
     const [, setAdirsAlignTimeSimVar] = useSimVar('L:A32NX_CONFIG_ADIRS_IR_ALIGN_TIME', 'Enum', Number.MAX_SAFE_INTEGER);
     const [dmcSelfTestTime, setDmcSelfTestTime] = usePersistentProperty('CONFIG_SELF_TEST_TIME', '12');
+    const [boardingRate, setBoardingRate] = usePersistentProperty('CONFIG_BOARDING_RATE', 'REAL');
     const [mcduInput, setMcduInput] = usePersistentProperty('MCDU_KB_INPUT', 'DISABLED');
     const [mcduTimeout, setMcduTimeout] = usePersistentProperty('CONFIG_MCDU_KB_TIMEOUT', '60');
-    const [boardingRate, setBoardingRate] = usePersistentProperty('CONFIG_BOARDING_RATE', 'REAL');
+    const [pauseAtTod, setPauseAtTod] = usePersistentProperty('PAUSE_AT_TOD', 'DISABLED');
+    const [todOffset, setTodOffset] = usePersistentNumberProperty('PAUSE_AT_TOD_DISTANCE', 10);
     const [realisticTiller, setRealisticTiller] = usePersistentNumberProperty('REALISTIC_TILLER_ENABLED', 0);
     const [homeCockpit, setHomeCockpit] = usePersistentProperty('HOME_COCKPIT_ENABLED', '0');
     const [autoFillChecklists, setAutoFillChecklists] = usePersistentNumberProperty('EFB_AUTOFILL_CHECKLISTS', 0);
@@ -136,6 +138,27 @@ export const RealismPage = () => {
                 <Toggle value={!!firstOfficerAvatar} onToggle={(value) => setFirstOfficerAvatar(value ? 1 : 0)} />
             </SettingItem>
 
+            <SettingGroup>
+                <SettingItem name={t('Settings.Realism.PauseAtTod')} unrealistic groupType="parent">
+                    <Toggle value={pauseAtTod === 'ENABLED'} onToggle={(value) => setPauseAtTod(value ? 'ENABLED' : 'DISABLED')} />
+                </SettingItem>
+                {pauseAtTod === 'ENABLED' && (
+                    <SettingItem name={t('Settings.Realism.PauseAtTodDistance')} groupType="sub">
+                        <SimpleInput
+                            className="text-center w-30"
+                            value={todOffset}
+                            min={0}
+                            max={50.0}
+                            disabled={(pauseAtTod !== 'ENABLED')}
+                            onChange={(event) => {
+                                if (!Number.isNaN(event) && parseInt(event) >= 0 && parseInt(event) <= 50.0) {
+                                    setTodOffset(parseFloat(event.trim()));
+                                }
+                            }}
+                        />
+                    </SettingItem>
+                )}
+            </SettingGroup>
         </SettingsPage>
     );
 };
